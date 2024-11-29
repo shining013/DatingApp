716a4678b1dad8336ac9046515b1b88c4084c2d3
shining013 <nevertheless113@naver.com>
2024-11-29 14:42:01 +0900

    chore: splashActivity

--- app/src/main/java/com/autoever/jamanchu/activities/SplashActivity.kt
+++ app/src/main/java/com/autoever/jamanchu/activities/SplashActivity.kt
@@ -4,12 +4,14 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.autoever.jamanchu.MainActivity
 import com.autoever.jamanchu.R
+import com.google.firebase.auth.FirebaseAuth
 
 
 class SplashActivity : AppCompatActivity() {
@@ -24,9 +26,18 @@ class SplashActivity : AppCompatActivity() {
         }
 
         Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this,IntroActivity::class.java))
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
-            finish()
+            val auth = FirebaseAuth.getInstance()
+            val currentUser = auth.currentUser
+            if (currentUser != null) {
+                Log.d("IntroActivity",currentUser.toString())
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                startActivity(Intent(this,IntroActivity::class.java))
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                finish()
+            }
         }, 2000)
     }
 }
