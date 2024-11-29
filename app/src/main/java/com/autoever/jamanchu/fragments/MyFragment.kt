ab948b062c6c94e9734b05f325fd3607929f42e4
shining013 <nevertheless113@naver.com>
2024-11-29 16:28:32 +0900

    feat: logout

--- app/src/main/java/com/autoever/jamanchu/fragments/MyFragment.kt
+++ app/src/main/java/com/autoever/jamanchu/fragments/MyFragment.kt
@@ -1,11 +1,15 @@
 package com.autoever.jamanchu.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.autoever.jamanchu.R
+import com.autoever.jamanchu.activities.IntroActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class MyFragment : Fragment() {
         override fun onCreateView(
@@ -13,6 +17,17 @@ class MyFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
         ): View? {
-            return inflater.inflate(R.layout.fragment_my, container, false)
+            val view = inflater.inflate(R.layout.fragment_my, container, false)
+
+            val textViewLogout = view.findViewById<TextView>(R.id.textViewLogout)
+            textViewLogout.setOnClickListener {
+                // Firebase 인증 로그아웃
+                FirebaseAuth.getInstance().signOut()
+
+                val intent = Intent(requireContext(), IntroActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                startActivity(intent)
+            }
+            return view
         }
 }
