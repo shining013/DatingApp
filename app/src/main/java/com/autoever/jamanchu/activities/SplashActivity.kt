f9216e97496f31f0fc5dc1eec3aecb80598b7d01
shining013 <nevertheless113@naver.com>
2024-11-29 10:46:10 +0900

    first commit

---
+++ app/src/main/java/com/autoever/jamanchu/activities/SplashActivity.kt
@@ -0,0 +1,31 @@
+package com.autoever.jamanchu.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.autoever.jamanchu.MainActivity
+import com.autoever.jamanchu.R
+
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this,IntroActivity::class.java))
+            finish()
+        }, 2000)
+    }
+}
