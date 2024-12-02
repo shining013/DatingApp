a3b416ce0931b78b5744abe931d796dc24196e56
shining013 <nevertheless113@naver.com>
2024-12-02 17:10:05 +0900

    feat: line

--- app/src/main/java/com/autoever/jamanchu/fragments/LineFragment.kt
+++ app/src/main/java/com/autoever/jamanchu/fragments/LineFragment.kt
@@ -6,14 +6,20 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.autoever.jamanchu.R
+import com.autoever.jamanchu.api.RetrofitInstance
 import com.autoever.jamanchu.models.Line
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class LineFragment : Fragment() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: LineAdapter
+    private val lines = mutableListOf<Line>()
         override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
@@ -24,18 +30,30 @@ class LineFragment : Fragment() {
             // 리사이클러뷰
             recyclerView = view.findViewById(R.id.recyclerView)
 
-            // 라인 리스트 더미 데이터
-            val lines = listOf(
-                Line(id = "1", user="Alice", line = "Hello, how are you?"),
-                Line(id = "2", user = "Bob", line = "I'm doing great, thanks!"),
-                Line(id = "3", user = "Charlie", line = "Let's catch up later.")
-            )
-
             adapter = LineAdapter(lines)
             recyclerView.adapter = adapter
             recyclerView.layoutManager = LinearLayoutManager(context)
 
+            fetchLines()
             return view
+        }
+
+        fun fetchLines() {
+            // 비동기 실행
+            lifecycleScope.launch {
+                try {
+                    val response = RetrofitInstance.api.getLines()
+                    if (response.isSuccessful && response.body() != null) {
+                        withContext(Dispatchers.Main) {
+                            lines.clear()
+                            lines.addAll(response.body()!!)
+                            adapter.notifyDataSetChanged()
+                        }
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
         }
 }
 
